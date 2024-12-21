8d5161a2111e15e0561fac0a13f57043db33a04a
anaguit <anasantoandre84@gmail.com>
2024-12-21 07:32:15 -0300

    implementa los resultados de express validator

--- back/src/controllers/authControllers.js
+++ back/src/controllers/authControllers.js
@@ -1,26 +1,32 @@
 const { Users } = require("../db");
+const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken")
 const { JWT_SECRET } = require("../config");
 const login = async ( req, res ) => {
     try {
-        const { UserName } = req.body
-        const userToLogin = await Users.findOne({ where:{ UserName } })
-        
-        if( !userToLogin ){
-            return res.status(404).json({ "msg": "El usuario no existe"});
-        }
-        const user = { userToLogin: userToLogin.id}
-        const createToken = (user) => {
-            return jwt.sign(user, JWT_SECRET, {
-              expiresIn: "7d",
+        const validationErrors = validationResult(req);
+        if ( validationErrors.isEmpty() ){
+            const { UserName } = req.body;
+            const userToLogin = await Users.findOne({ where:{ UserName } });
+
+            const user = { userToLogin: userToLogin.Id}
+            
+            const createToken = (user) => {
+                return jwt.sign(user, JWT_SECRET, {
+                expiresIn: "7d",
+                });
+            };
+            const token = createToken(user);
+            res.status(200).header("authorization", token).json({
+                "msg": "logueo exitoso",
+                token: token
             });
-        };
-        const token = createToken(user);
-        res.status(200).header("authorization", token).json({
-            "msg": "logueo exitoso",
-            token: token
-          });
-        
+        } else {
+            res.status(400).json({
+                msg: "No se pudo ingresar porque no cumple las validaciones",
+                data: validationErrors,
+            });
+        };      
     } catch (error) {
         console.log(error);
         res.status(500).json({ "msg":"Error al loguearse"})
